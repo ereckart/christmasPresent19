063f000f56392b36340a944375914c3f6bdd89bb
Kevin Thomas <thomaskevinlee@gmail.com>
2019-12-11 03:12:36 -0800

    added server notification and broadcast

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ var io = require('socket.io')(http);
 
 // MAZE CODE
 app.get('/maze', function(req, res){
-  res.sendFile(__dirname + '/html/maze.html');
+    res.sendFile(__dirname + '/html/maze.html');
 });
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('movement', function(msg){
-    console.log('message: ' + msg);
-  });
+    console.log('a user connected');
+    socket.on('direction', function(msg){
+        socket.broadcast.emit('direction', msg)
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
