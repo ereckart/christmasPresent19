024053640a663fc7deb420a4860d5204a64e54bb
Kevin Thomas <thomaskevinlee@gmail.com>
2020-05-05 15:46:03 -0400

    switched to generic page with ejs rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+app.use(express.urlencoded());
+app.use(express.json());
 
 // MAZE CODE
 app.get('/maze', function(req, res){
@@ -11,6 +16,13 @@ io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('direction', function(msg){
         socket.broadcast.emit('direction', msg)
+    });
+});
+app.get('/g/:player/:place', function(req, res){
+    console.log(req.params.place);
+    res.render('genericGamePage', {
+        'player': req.params.player,
+        'place': req.params.place
     });
 });
 
